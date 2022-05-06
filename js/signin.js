b2503e8442dd1bbf799a65e40017965127849998
devjoshua312 <72260184+devjoshua312@users.noreply.github.com>
2022-05-06 21:57:11 +0530

    update files

---
+++ js/signin.js
@@ -0,0 +1,30 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+// const loginErrorMsg = document.getElementById("login-error-msg");
+
+
+loginButton.addOnClickListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "joshua" && password === "testing") {
+        alert("You have successfully logged in");
+        location.reload();
+    } else {
+        alert("Invalid username or password");
+    }
+})
+
+// loginButton.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     const username = loginForm.username.value;
+//     const password = loginForm.password.value;
+
+//     if (username === "user" && password === "web") {
+//         alert("You have successfully logged in.");
+//         location.reload();
+//     } else {
+//         loginErrorMsg.style.opacity = 1;
+//     }
+// })
