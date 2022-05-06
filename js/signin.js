fc29d8f0b6bdd19935a76d13bcbb599e96524376
devjoshua312 <72260184+devjoshua312@users.noreply.github.com>
2022-05-06 22:29:00 +0530

    added iplog.js

--- js/signin.js
+++ js/signin.js
@@ -2,7 +2,6 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 // const loginErrorMsg = document.getElementById("login-error-msg");
 
-
 loginButton.addOnClickListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
